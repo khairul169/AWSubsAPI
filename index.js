9b2d67321e49209cc719119c2c35c63e6013b67d
Khairul Hidayat <me@khairul.my.id>
2019-11-14 20:58:17 +0700

    Update anime listing route

--- index.js
+++ index.js
@@ -1,54 +1,72 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const config = require('./config');
-const cacheMiddleware = require('./cache-middleware');
-const state = require('./state');
+const config = require("./config");
+const cacheMiddleware = require("./cache-middleware");
+const state = require("./state");
 
-const getLatest = require('./routes/latest');
-const anime = require('./routes/anime');
-const getRelease = require('./routes/release');
+const getLatest = require("./routes/latest");
+const anime = require("./routes/anime");
+const getRelease = require("./routes/release");
 
-const leechAwsubs = require('./leecher/short-awsubs');
+const leechAwsubs = require("./leecher/short-awsubs");
 
-app.all('*', cacheMiddleware);
+app.all("*", cacheMiddleware);
 
-app.all('*', (req, res, next) => {
-  state.address = req.protocol + '://' + req.get('host') + '/';
+app.all("*", (req, res, next) => {
+  state.address = req.protocol + "://" + req.get("host") + "/";
   next();
 });
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const items = await getLatest();
   res.json(items);
 });
 
-app.get('/page/:page', async (req, res) => {
+app.get("/page/:page", async (req, res) => {
   const items = await getLatest(req.params.page);
   res.json(items);
 });
 
-app.get('/anime/', async (req, res) => {
-  const list = await anime.getAnimeList();
+app.get("/anime/", async (req, res) => {
+  const list = await anime.getAnimeList({});
   res.json(list);
 });
 
-app.get('/anime/:anime', async (req, res) => {
+app.get("/anime/page/:page", async (req, res) => {
+  const info = await anime.getAnimeList({ page: req.params.page });
+  res.json(info);
+});
+
+app.get("/genre/:genre", async (req, res) => {
+  const info = await anime.getAnimeList({ genre: req.params.genre });
+  res.json(info);
+});
+
+app.get("/genre/:genre/page/:page", async (req, res) => {
+  const info = await anime.getAnimeList({
+    genre: req.params.genre,
+    page: req.params.page
+  });
+  res.json(info);
+});
+
+app.get("/anime/:anime", async (req, res) => {
   const info = await anime.getAnime(req.params.anime);
   res.json(info);
 });
 
-app.get('/release/:id', async (req, res) => {
+app.get("/release/:id", async (req, res) => {
   const data = await getRelease(req.params.id);
   res.json(data);
 });
 
-app.get('/get/:url', async (req, res) => {
-  const url = Buffer.from(req.params.url, 'base64').toString('ascii');
+app.get("/get/:url", async (req, res) => {
+  const url = Buffer.from(req.params.url, "base64").toString("ascii");
   const result = await leechAwsubs.tryLeech(url);
   res.redirect(result);
 });
 
 const server = app.listen(config.PORT, () => {
   const address = server.address();
-  console.log('Server started.', address);
+  console.log("Server started.", address);
 });
--- routes/anime.js
+++ routes/anime.js
@@ -1,113 +1,150 @@
-const axios = require('axios').default;
-const cheerio = require('cheerio');
-const consts = require('../consts');
+const axios = require("axios").default;
+const cheerio = require("cheerio");
+const consts = require("../consts");
 
-const onAnimeList = (body) => {
-    let $ = cheerio.load(body);
+const onAnimeList = body => {
+  let $ = cheerio.load(body);
 
-    const items = [];
+  const items = [];
 
-    $('.soralist .series').map((index, element) => {
-        const item = $(element);
+  $(".arche").map((index, element) => {
+    const item = $(element);
 
-        const title = item.text();
-        const url = item.attr('href');
-        const id = url.split('/')[4];
-
-        items.push({ id, title, url });
+    const title = item.find("h2 a").text();
+    const image = item.find("img").attr("src");
+    const url = item.find("h2 a").attr("href");
+    const id = url.split("/")[4];
+    const type = item.find("a span").text();
+    const genres = [];
+    const tags = item.find("span a[rel=tag]").map((i, el) => {
+      const elem = $(el);
+      const name = elem.text();
+      const url = elem.attr("href").split("/");
+      const id = url[url.length - 2];
+      genres.push({ id, name });
     });
 
-    return items;
-}
+    items.push({ id, title, image, url, type, genres });
+  });
 
-const getAnimeList = async (anime) => {
-    try {
-        let url = 'https://awsubs.tv/anime/?list';
-        let response = await axios.get(url);
+  return items;
+};
 
-        return {
-            status: 0,
-            result: onAnimeList(response.data)
-        };
-    } catch (e) {
-        return e.response && e.response.status === 404 ? consts.ERROR_404 : consts.ERROR_UNEXPECTED;
+const getAnimeList = async ({ page, genre }) => {
+  try {
+    let url = "";
+
+    if (genre) {
+      url =
+        `https://awsubs.tv/genre-anime/${genre}/` +
+        (page ? `page/${page}` : "");
+    } else {
+      url = "https://awsubs.tv/anime/" + (page ? `page/${page}` : "");
     }
-}
+
+    // fetch data
+    let response = await axios.get(url);
+
+    return {
+      status: 0,
+      result: onAnimeList(response.data)
+    };
+  } catch (e) {
+    return e.response && e.response.status === 404
+      ? consts.ERROR_404
+      : consts.ERROR_UNEXPECTED;
+  }
+};
 
 const parseAnimeInfo = (name, value, info) => {
-    switch (name) {
-        case 'Japanese':
-            info.japanese = value;
-        case 'Type':
-            info.type = value;
-        case 'Status':
-            info.status = value;
-        case 'Episodes':
-            info.episodes = value;
-        case 'Producers':
-            info.producers = value;
-        case 'Genres':
-            info.genres = value;
-        case 'Duration':
-            info.duration = value;
-        case 'Score':
-            info.score = value;
-        case 'Views':
-            info.views = value;
-        case 'Posted by':
-            info.author = value;
-        case 'Posted on':
-            info.date = value;
-        default:
-            break;
-    }
-    return info;
-}
+  switch (name) {
+    case "Japanese":
+      info.japanese = value;
+    case "Type":
+      info.type = value;
+    case "Status":
+      info.status = value;
+    case "Episodes":
+      info.episodes = value;
+    case "Producers":
+      info.producers = value;
+    case "Genres":
+      info.genres = value;
+    case "Duration":
+      info.duration = value;
+    case "Score":
+      info.score = value;
+    case "Views":
+      info.views = value;
+    case "Posted by":
+      info.author = value;
+    case "Posted on":
+      info.date = value;
+    default:
+      break;
+  }
+  return info;
+};
 
-const onLoaded = (body) => {
-    let $ = cheerio.load(body);
+const onLoaded = body => {
+  let $ = cheerio.load(body);
 
-    const title = $('article h1').text();
-    const image = $('article .imgprop img').attr('src');
-    const info = {};
-    const items = [];
+  const title = $("article h1").text();
+  const image = $("article .imgprop img").attr("src");
+  const info = {};
+  const items = [];
 
-    $('article ul').children('li').map((index, element) => {
-        const item = $(element);
+  $("article ul")
+    .children("li")
+    .map((index, element) => {
+      const item = $(element);
 
-        const title = item.find("span a").text().trim();
-        const url = item.find("span a").attr('href');
-        const id = typeof url == 'string' && url.split('/')[3];
+      const title = item
+        .find("span a")
+        .text()
+        .trim();
+      const url = item.find("span a").attr("href");
+      const id = typeof url == "string" && url.split("/")[3];
 
-        title.length && items.push({ id, title, url });
+      title.length && items.push({ id, title, url });
     });
 
-    $('article .listinfo tbody').children('tr').map((index, element) => {
-        const item = $(element);
+  $("article .listinfo tbody")
+    .children("tr")
+    .map((index, element) => {
+      const item = $(element);
 
-        const name = item.find('th').text().trim();
-        const value = item.find('td').text().trim();
-        parseAnimeInfo(name, value, info);
+      const name = item
+        .find("th")
+        .text()
+        .trim();
+      const value = item
+        .find("td")
+        .text()
+        .trim();
+      parseAnimeInfo(name, value, info);
     });
 
-    return { title, image, info, items };
-}
+  return { title, image, info, items };
+};
 
-const getAnime = async (anime) => {
-    try {
-        let url = 'https://awsubs.tv/anime/' + anime + '/';
-        let response = await axios.get(url);
+const getAnime = async anime => {
+  try {
+    let url = "https://awsubs.tv/anime/" + anime + "/";
+    let response = await axios.get(url);
 
-        return {
-            status: 0,
-            result: onLoaded(response.data)
-        };
-    } catch (e) {
-        return e.response && e.response.status === 404 ? consts.ERROR_404 : consts.ERROR_UNEXPECTED;
-    }
-}
+    return {
+      status: 0,
+      result: onLoaded(response.data)
+    };
+  } catch (e) {
+    return e.response && e.response.status === 404
+      ? consts.ERROR_404
+      : consts.ERROR_UNEXPECTED;
+  }
+};
 
 module.exports = {
-    getAnimeList,
-    getAnime
+  getAnimeList,
+  getAnime
 };
